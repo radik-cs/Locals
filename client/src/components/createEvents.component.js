082aafed9eb77faaa02e648d86febd37128ec6a7
Matt DiPaolo <matt@dldipaolo.com>
2021-11-17 00:48:38 -0500

    added nested navigation to homepage

--- client/src/components/createEvents.component.js
+++ client/src/components/createEvents.component.js
@@ -1,32 +1,9 @@
 export default function CreateEvents() {
     return (
-        <form >
-            <h3>Sign In</h3>
+        <div>
+            <h3>create event</h3>
+        </div>
 
-            <div className="form-group">
-                <label>Username</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Enter username"
-                   
-                />
-            </div>
-
-            <div className="form-group">
-                <label>Password</label>
-                <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Enter password"
-                    
-                />
-            </div>
-
-            <p> </p>
-
-            <button type="submit" className="btn btn-primary btn-block">Submit</button>
-        </form>
     );
 
 }
