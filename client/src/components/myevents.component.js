c455d515429f7e6304686d7c52097a252fa459ba
Matt DiPaolo <matt@dldipaolo.com>
2021-11-17 11:04:19 -0500

    configured front end requests and backend routes to retrieve a user's events

--- client/src/components/myevents.component.js
+++ client/src/components/myevents.component.js
@@ -1,7 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState} from 'react'
+const axios = require("axios")
 
 
 export default function MyEvents(props){
+    const [events, setEvents] = useState([])
+    const username = props.username
+
+
+    useEffect(async () => {
+        let response = await axios.post("/api/events/get-events",{username})
+        console.log(response.data)
+        
+        
+    })
     return (
         <h1>hello</h1>
     )
--- routes/api/events.route.js
+++ routes/api/events.route.js
@@ -14,4 +14,15 @@ router.post("/", async (req, res) => {
     return res.json(errors)
 });
 
+router.post("/get-events", async (req, res) => {
+    const db = MongoUtil.getDB();
+    const events = await db.collection('events')
+    console.log("requested user events")
+    const username = req.body.username
+    let query = {username : `${username}`}
+    const result = await events.find(query).toArray()
+    console.log(result)
+    return res.json(result)
+});
+
 module.exports = router;
